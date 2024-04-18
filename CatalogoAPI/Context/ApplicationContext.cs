7069ae0d33d9082de9dc849eae3034659a475589
GustavoHCCampos <gustavohccampos@gmail.com>
2024-04-17 21:22:40 -0300

    AspNet API

---
+++ CatalogoAPI/Context/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using CatalogoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoAPI.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+
+    }
+}
---
+++ CatalogoAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,82 @@
+﻿using CatalogoAPI.Context;
+using CatalogoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public ProdutosController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados!");
+            }
+
+            return produtos;
+        }
+
+        [HttpGet("{id:int}", Name = "ObterProduto")]
+        public ActionResult<Produto> GetId(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado!");
+            }
+            return produto;
+        }
+
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            { return BadRequest(); }
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId) { return BadRequest(); }
+            _context.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            //var produto = _context.Produtos.Find(id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto ndo localizado...");
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(produto);
+        }
+    }
+}
+
